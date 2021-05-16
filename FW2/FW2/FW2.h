5775b9cee1f620916430854a06d019a506b329cd
JUAN MARTELL NAVARRETE BERRUETE <jmnavarrete@bancoazteca.comls>
2021-05-16 01:13:18 -0500

    second FW and scheme

---
+++ FW2/FW2/FW2.h
@@ -0,0 +1,18 @@
+//
+//  FW2.h
+//  FW2
+//
+//  Created by Juan Martell on 16/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FW2.
+FOUNDATION_EXPORT double FW2VersionNumber;
+
+//! Project version string for FW2.
+FOUNDATION_EXPORT const unsigned char FW2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FW2/PublicHeader.h>
+
+
