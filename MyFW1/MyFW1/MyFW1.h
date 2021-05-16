f5298744e0a455c92a475c9bb8a290ccfae61c68
JUAN MARTELL NAVARRETE BERRUETE <jmnavarrete@bancoazteca.comls>
2021-05-16 00:59:14 -0500

    update add front and fw1

---
+++ MyFW1/MyFW1/MyFW1.h
@@ -0,0 +1,18 @@
+//
+//  MyFW1.h
+//  MyFW1
+//
+//  Created by Juan Martell on 16/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFW1.
+FOUNDATION_EXPORT double MyFW1VersionNumber;
+
+//! Project version string for MyFW1.
+FOUNDATION_EXPORT const unsigned char MyFW1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFW1/PublicHeader.h>
+
+
---
+++ MyTest/MyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTest
+//
+//  Created by Juan Martell on 16/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
